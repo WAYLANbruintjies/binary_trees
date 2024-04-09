d138e4f7e2265f9d7433c773fae043bfde6a2886
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-09 14:45:08 -0700

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,7 +6,7 @@
  * @value: value to store in the new node
  * Return: pointer to the new node or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
