f1171cc3b8ab7c6e0e1ca494bc4309da2d248842
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-09 14:27:54 -0700

    modified

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left != NULL)
 	{
+		new->n = value;
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
