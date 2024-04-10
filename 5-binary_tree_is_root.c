149d9fa29e405b8da11803ec635c719a421e6e26
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-10 05:45:33 -0700

    Is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a root
+ * @node: pointer to the node to check
+ * Return: 1 (SUCCESS) if node is a root, otherwise 0 (FAIL)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (0);
+
+	return (1);
+}
