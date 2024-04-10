7b32490b1dd517216c44e2d59c6ada7bebfecb6f
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-04-10 13:15:32 -0700

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,6 +14,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (1);
 	else
 	{
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+		return (binary_tree_leaves(tree->left) + 
+			binary_tree_leaves(tree->right));
 	}
 }
